ddacf3c9023a47740494a79f464069a6b7288120
Jonathan Kicillof <jkicillof@manas.com.ar>
2020-11-04 13:15:28 -0300

    partial commit

--- components/Handbook.js
+++ components/Handbook.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from 'react';
 import ReactMarkdown from 'react-markdown'
 import footnotes from 'remark-footnotes'
 import Nav from './Nav'
@@ -8,153 +9,37 @@ import styles from '../styles/Handbook.module.css'
 
 function Handbook({ contents }) {
 
-    function handleClick(section) {
-        console.log(section.ref)
-        console.log(section.ref.offsetTop)
+    const [current, setCurrent] = useState()
+
+    function scrollTo(section) {
         section.ref.scrollIntoView({
             behavior: 'smooth',
             block: 'start',
         })
-        // window.scrollTo({
-        //     behavior: 'smooth',
-        //     top: section.ref.offsetTop,
-        // })
     }
 
-    // function handleScroll(e) {
-    //     let current = document.querySelector('.current')
-    //     if(current) {
-    //         current.classList.remove('current')
-    //     }
-    //     current = sections.find(section => {
-    //         return section.offsetTop <= window.scrollY && window.scrollY < section.offsetTop + section.clientHeight
-    //     })
-    //     if(current) {
-    //         document.querySelector(`#reference-${current.id}`).classList.add('current')
-    //     }
-    // }
-    /*
+    useEffect(() => {
+        const handleScroll = () => {
+          const selected = contents.find(({ref}) => ref.offsetTop <= window.scrollY && window.scrollY < ref.offsetTop + ref.clientHeight)
+          if (selected !== current) {
+            setCurrent(selected);
+          } else if (!selected && current) {
+            setCurrent(undefined);
+          }
+        };
     
-    
-    
-cover
-legal    
-nav
-internal links
-footnotes
-preface
-readme
+        handleScroll();
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+          window.removeEventListener("scroll", handleScroll);
+        };
+      }, [current]);
+// internal links
+// get images outside p
+// review content
 
-
-import React, { useRef, useEffect, useState } from "react";
-import "./App.css";
-
-const getDimensions = ele => {
-  const { height } = ele.getBoundingClientRect();
-  const offsetTop = ele.offsetTop;
-  const offsetBottom = offsetTop + height;
-
-  return {
-    height,
-    offsetTop,
-    offsetBottom,
-  };
-};
-
-const scrollTo = ele => {
-  ele.scrollIntoView({
-    behavior: "smooth",
-    block: "start",
-  });
-};
-
-function App() {
-  const [visibleSection, setVisibleSection] = useState();
-
-  const headerRef = useRef(null);
-  const leadershipRef = useRef(null);
-  const providerRef = useRef(null);
-  const operationsRef = useRef(null);
-
-  const sectionRefs = [
-    { section: "Leadership", ref: leadershipRef },
-    { section: "Providers", ref: providerRef },
-    { section: "Operations", ref: operationsRef },
-  ];
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const { height: headerHeight } = getDimensions(headerRef.current);
-      const scrollPosition = window.scrollY + headerHeight;
-
-      const selected = sectionRefs.find(({ section, ref }) => {
-        const ele = ref.current;
-        if (ele) {
-          const { offsetBottom, offsetTop } = getDimensions(ele);
-          return scrollPosition > offsetTop && scrollPosition < offsetBottom;
-        }
-      });
-
-      if (selected && selected.section !== visibleSection) {
-        setVisibleSection(selected.section);
-      } else if (!selected && visibleSection) {
-        setVisibleSection(undefined);
-      }
-    };
-
-    handleScroll();
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [visibleSection]);
-  return (
-    <div className="App">
-      <div className="top-spacer" />
-
-      <div className="content">
-        <div className="sticky">
-          <div className="header" ref={headerRef}>
-            <button
-              type="button"
-              className={`header_link ${visibleSection === "Leadership" ? "selected" : ""}`}
-              onClick={() => {
-                scrollTo(leadershipRef.current);
-              }}
-            >
-              Leadership
-            </button>
-            <button
-              type="button"
-              className={`header_link ${visibleSection === "Providers" ? "selected" : ""}`}
-              onClick={() => {
-                scrollTo(providerRef.current);
-              }}
-            >
-              Providers
-            </button>
-            <button
-              type="button"
-              className={`header_link ${visibleSection === "Operations" ? "selected" : ""}`}
-              onClick={() => {
-                scrollTo(operationsRef.current);
-              }}
-            >
-              Operations
-            </button>
-          </div>
-        </div>
-        <div className="section" id="Leadership" ref={leadershipRef} />
-        <div className="section" id="Providers" ref={providerRef} />
-        <div className="section" id="Operations" ref={operationsRef} />
-      </div>
-
-      <div className="bottom-spacer" />
-    </div>
-  );
-}
-
-export default App;*/
+// change file names 
+// footnotes
 
     const renderers = {
         image: Illustration,
@@ -164,7 +49,7 @@ export default App;*/
     // console.log(contents)
     return (
         <div className={styles.handbook}>
-            <Nav contents={contents} handleClick={handleClick}></Nav>
+            <Nav current={current} contents={contents} scrollTo={scrollTo}></Nav>
             {/* <section id='handbook'>
                 <div class='logo'></div>
                 <div class='picture' style='background-image:url(images/ceci-nest-pas-une-societe-de-logiciels.svg);'></div>
@@ -186,7 +71,7 @@ export default App;*/
             <p>This text serves two simultaneous purposes that mutually feedback each other: it is a user manual which explains how some processes work inside Manas and it's also an instruction manual, as it describes certain goals we want to achieve. True to our principles, the text is in a repository where you can comment on it and send your change proposals. This text will always be a beta version. If you agree with a chapter of the manual, but you don't think it is being implemented correctly in reality, let your voice be heard loud and clear and help us correct what we can. If you disagree with parts of the manual, please suggest the change you'd like to see and help us implement it.</p>
         </section> */}
             {contents.map((section, i) => (
-                <section key={i} id={i} ref={ref => section.ref = ref}>
+                <section key={i} ref={ref => section.ref = ref}>
                     <ReactMarkdown escapeHtml={false} renderers={renderers} source={section.content} />
                 </section>
             ))}
--- components/Nav.js
+++ components/Nav.js
@@ -1,12 +1,32 @@
+import React, {useEffect} from 'react';
 import styles from '../styles/Nav.module.css'
 
-function Nav({ contents, handleClick }) {
+function Nav({ current, contents, scrollTo }) {
+
+    let container
+
+    useEffect(() => {
+        if(current) {
+            if(current.nav.offsetTop < container.scrollTop) {
+                container.scrollTo({
+                    behavior: 'smooth',
+                    top: current.nav.offsetTop
+                })
+            } else if(container.scrollTop + container.clientHeight < current.nav.offsetTop + current.nav.clientHeight ) {
+                container.scrollTo({
+                    behavior: 'smooth',
+                    top: current.nav.offsetTop + current.nav.clientHeight - container.clientHeight
+                })
+            }
+        }
+    });
+
     return (
-        <nav className={styles.nav}>
+        <nav ref={ref => container = ref} className={styles.nav}>
             {
                 contents.map((entry, i) => 
-                    <div key={i}>
-                        <a style={{paddingLeft: `${18 * entry.level}px`}} id={`ref-${i}`} href='#' onClick={e => handleClick(entry)}>{entry.data.title}</a>
+                    <div key={i} ref={ref => entry.nav = ref} className={entry === current? styles.current : null}>
+                        <a style={{paddingLeft: `${18 * entry.level}px`}} onClick={e => scrollTo(entry)}>{entry.data.title}</a>
                     </div>
                 )
             }
