671d843137dc43cbbf0e363c1439dc51afa14cbb
Jonathan Kicillof <jkicillof@manas.com.ar>
2020-12-28 09:56:43 -0300

    Fix invalid DOM

--- components/FootnoteDefinition.js
+++ components/FootnoteDefinition.js
@@ -1,7 +1,7 @@
 import styles from '../styles/FootnoteDefinition.module.css'
 
 function FootnoteDefinition({identifier, label, children}) {
-    return <p key={identifier} id={`fn:${identifier}`} className={styles.footnoteDefinition}><span>{`${label}.`}</span><a href={`#fnref:${identifier}`} role='doc-backlink'>{children}</a></p>
+    return <div key={identifier} id={`fn:${identifier}`} className={styles.footnoteDefinition}><span>{`${label}.`}</span><a href={`#fnref:${identifier}`} role='doc-backlink'>{children}</a></div>
 }
 
 export default FootnoteDefinition
